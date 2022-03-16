f68bbace2a6bc7f7b48985024b63ac347d012111
RockRagnar12 <88911770+RockRagnar12@users.noreply.github.com>
2022-03-15 20:29:14 -0500

    Add files via upload

--- handler.js
+++ handler.js
@@ -440,27 +440,6 @@ ${global.owner.map((v, i) => '*Contacto ' + (i + 1) + ':* wa.me/' + v).join('\n'
     let chat = global.DATABASE._data.chats[jid] || {}
     let text = ''
     switch (action) {
-      case 'add':
-      case 'remove':
-        if (chat.welcome) {
-          let groupMetadata = await this.groupMetadata(jid)
-          for (let user of participants) {
-            let pp = './undefined.jpg'
-            try {
-              pp = await this.getProfilePicture(user)
-            } catch (e) {
-            } finally {
-              text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'welcome, @user!').replace('@subject', await this.getName(jid)).replace('@desc', groupMetadata.desc) :
-                (chat.sBye || this.bye || conn.bye || 'bye, @user!')).replace('@user', '@' + user.split('@')[0])
-              this.sendFile(jid, pp, 'pp.jpg', text, null, false, {
-                contextInfo: {
-                  mentionedJid: [user]
-                }
-              })
-            }
-          }
-        }
-        break
       case 'promote':
         text = (chat.sPromote || this.spromote || conn.spromote || '@user ```ahora es administrador```')
       case 'demote':
